b489c7e53e2471ed4576f0f6bdbbf1ce1544ec48
PedroLFPrado <pedro.lfprado@gmail.com>
2025-09-06 13:44:33 -0300

    Correção culling

--- backfaceculling.py
+++ backfaceculling.py
@@ -2,74 +2,91 @@ import pygame
 from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
-from math import sin, cos
 
-angle = 0
-culling = True
+# Vértices do cubo
+vertices = [
+    [1, 1, -1],
+    [1, -1, -1],
+    [-1, -1, -1],
+    [-1, 1, -1],
+    [1, 1, 1],
+    [1, -1, 1],
+    [-1, -1, 1],
+    [-1, 1, 1]
+]
 
-def init():
-    glEnable(GL_DEPTH_TEST)
-    glClearColor(0.11, 0.11, 0.13, 1.0)
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    gluPerspective(60, 800/600, 0.1, 100)
-    glMatrixMode(GL_MODELVIEW)
+# Faces do cubo
+faces = [
+    (0, 1, 2, 3),  # trás
+    (4, 5, 6, 7),  # frente
+    (0, 1, 5, 4),  # direita
+    (2, 3, 7, 6),  # esquerda
+    (0, 3, 7, 4),  # cima
+    (1, 2, 6, 5)   # baixo
+]
 
-def torus(R=1.2, r=0.4, ns=48, nt=24):
-    
-    for i in range(ns):
-        glBegin(GL_QUAD_STRIP)
-        for j in range(nt+1):
-            for k in (i, i+1):
-                s = (k/ns)*2.0*3.14159
-                t = (j/nt)*2.0*3.14159
-                x = (R + r*cos(t))*cos(s)
-                y = (R + r*cos(t))*sin(s)
-                z = r*sin(t)
-                nx = cos(t)*cos(s)
-                ny = cos(t)*sin(s)
-                nz = sin(t)
-                glNormal3f(nx, ny, nz)
-                glVertex3f(x, y, z)
-        glEnd()
+# Cores diferentes para cada face
+colors = [
+    (1, 0, 0),  # vermelho
+    (0, 1, 0),  # verde
+    (0, 0, 1),  # azul
+    (1, 1, 0),  # amarelo
+    (1, 0, 1),  # magenta
+    (0, 1, 1)   # ciano
+]
+
+def draw_cube():
+    glBegin(GL_QUADS)
+    for i, face in enumerate(faces):
+        glColor3fv(colors[i])
+        for vertex in face:
+            glVertex3fv(vertices[vertex])
+    glEnd()
 
 def main():
-    global angle, culling
     pygame.init()
-    pygame.display.set_mode((800,600), DOUBLEBUF|OPENGL)
-    pygame.display.set_caption("Back-Face Culling")
-    init()
-    clock = pygame.time.Clock()
+    display = (800, 600)
+    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
 
-   
-    glEnable(GL_LIGHTING); glEnable(GL_LIGHT0)
-    glLightfv(GL_LIGHT0, GL_POSITION, (3,4,5,1))
-    glLightfv(GL_LIGHT0, GL_DIFFUSE,  (1,1,1,1))
-    glLightfv(GL_LIGHT0, GL_SPECULAR, (1,1,1,1))
-    glMaterialfv(GL_FRONT, GL_SPECULAR, (1,1,1,1))
-    glMaterialf(GL_FRONT, GL_SHININESS, 40)
+    glEnable(GL_DEPTH_TEST)
+    glEnable(GL_CULL_FACE)  # culling
+    glCullFace(GL_BACK)     # começa eliminando faces de trás
+    glFrontFace(GL_CCW)     # ordem dos vértices: anti-horário
+
+    gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
+    glTranslatef(0.0, 0.0, -5)
+
+    cull_modes = [GL_BACK, GL_FRONT, GL_FRONT_AND_BACK]
+    cull_names = ["GL_BACK", "GL_FRONT", "GL_FRONT_AND_BACK"]
+    current_mode = 0
+
+    pygame.display.set_caption(f"Culling: {cull_names[current_mode]}")
+
+    angle = 0
 
     running = True
     while running:
-        for e in pygame.event.get():
-            if e.type==QUIT or (e.type==KEYDOWN and e.key==K_ESCAPE):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 running = False
-            if e.type==KEYDOWN and e.key==K_c:
-                culling = not culling
-                if culling:
-                    glEnable(GL_CULL_FACE); glCullFace(GL_BACK)
-                else:
-                    glDisable(GL_CULL_FACE)
+            if event.type == KEYDOWN:
+                if event.key == K_c:  # troca o modo de culling
+                    current_mode = (current_mode + 1) % len(cull_modes)
+                    glCullFace(cull_modes[current_mode])
+                    pygame.display.set_caption(f"Culling: {cull_names[current_mode]}")
 
-        angle += 0.5
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        glLoadIdentity()
-        gluLookAt(0,0,6, 0,0,0, 0,1,0)
-        glRotatef(angle, 0,1,0)
-        glColor3f(0.8, 0.8, 0.9)
-        torus()
+
+        glPushMatrix()
+        glRotatef(angle, 1, 1, 0)
+        draw_cube()
+        glPopMatrix()
+
+        angle += 1
+
         pygame.display.flip()
-        clock.tick(60)
+        pygame.time.wait(20)
+
     pygame.quit()
 
 if __name__ == "__main__":
